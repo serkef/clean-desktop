ac5103937d79ab3f4d5c74437d7afe8818d7d85f
serkef <sergiokef@gmail.com>
2018-04-26 14:54:39 +0200

    changed params to arguments

--- workfolderCleaner.py
+++ workfolderCleaner.py
@@ -4,15 +4,8 @@ import datetime
 import shutil
 from contextlib import contextmanager
 from glob import glob
-
-# Setup path
-cutoff_limit = 30
-ts_format = '%Y-%m-%d'
-desktop = os.path.realpath('')
-desktop_exceptions = ['']
-workfolder = os.path.realpath('')
-workfolder_archive = os.path.join(workfolder, 'ARCHIVE')
-workfolder_today = os.path.join(workfolder, str(datetime.date.today().strftime(ts_format)))
+import argparse
+from pathlib import Path
 
 
 @contextmanager
@@ -31,7 +24,7 @@ def super_archive(sources, destination):
             print('Archived %s to %s' % (source, destination))
 
 
-def is_old_directory(path):
+def is_old_directory(path, cutoff_limit):
     """Returns True if path was last modified > 30 days ago. False otherwise."""
     if not isdir(path):
         return False
@@ -41,10 +34,10 @@ def is_old_directory(path):
             return (datetime.date.today() - modifDate.date()).days > cutoff_limit
 
 
-def archive_daily_workfolders(dirsFromPath, archivePath):
+def archive_daily_workfolders(dirsFromPath, archivePath, cutoff_limit):
     """Archives all directories in dirsFromPath to workfolder_archive, as long as their modification date
     was prior to cutoff_limit global var"""
-    dirs = [d for d in glob(os.path.join(dirsFromPath, '*')) if is_old_directory(path=d)]
+    dirs = [d for d in glob(os.path.join(dirsFromPath, '*')) if is_old_directory(path=d, cutoff_limit=cutoff_limit)]
     super_archive(sources=dirs, destination=archivePath)
 
 
@@ -70,8 +63,57 @@ def archive_desktop_items(fromPath, toPath, exceptions):
 
 
 def main():
+    desc = '''Workfolder & Desktop Cleaner
+    Archives your desktop files to the last day Workfolder, creates a new Workfolder and archives old workfolders
+    that exceed the cutoff limit.'''
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument(
+        '-workfolderf',
+        help="Main Workfolder. One subdirectory will be created, every day this script is run.",
+        type=Path
+    )
+    parser.add_argument(
+        '-archive',
+        help="Archive directory name. Will be a subdirectory of Workfolder. Defaults to ARCHIVE.",
+        default='ARCHIVE',
+        type=str
+    )
+    parser.add_argument(
+        '-desktop',
+        help="Desktop directory. Files will be moved from there to Workfolder.",
+        type=Path
+    )
+    parser.add_argument(
+        '-cutoff',
+        help="Cutoff limit in days. Any directory with modification date prior to this will be moved to archive. "
+             "Defaults to 30 days.",
+        default=30,
+        type=int
+    )
+    parser.add_argument(
+        '-ds_fmt',
+        help="Datestamp format for workfolder created directory. Defaults to: '%Y-%m-%d'",
+        default='%Y-%m-%d',
+        type=int
+    )
+    parser.add_argument(
+        '-exceptions',
+        help="Except desktop files list. files seperated with comma",
+        type=str
+    )
+    args = parser.parse_args()
+
+    workfolder = str(args.workfolderf)
+    workfolder_archive = os.path.join(workfolder, args.archive)
+    cutoff_limit = str(args.cutoff_limit)
+    ds_fmt = str(args.ds_fmt)
+    desktop = str(args.desktop)
+    desktop_exceptions = args.exceptions.split(',')
+    workfolder_today = os.path.join(workfolder, str(datetime.date.today().strftime(ds_fmt)))
+
     # Archive old folders
-    archive_daily_workfolders(dirsFromPath=workfolder, archivePath=workfolder_archive)
+    archive_daily_workfolders(dirsFromPath=workfolder, archivePath=workfolder_archive, cutoff_limit=cutoff_limit)
 
     # Delete empty folders
     remove_empty_folders(fromPath=workfolder)
