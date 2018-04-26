eb918b5b188ca125688fa1a86c22a440486420bc
serkef <sergiokef@gmail.com>
2018-04-26 16:04:41 +0200

    refactoring, comments

--- workfolderCleaner.py
+++ workfolderCleaner.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 def archive_daily_workfolders(workfolder: Path, archive: Path, cutoff_limit: int) -> None:
     """Archives all directories in workfolder to workfolder_archive, as long as their modification date
-    was prior to cutoff_limit global var"""
+    was prior to cutoff limit."""
     for dir in workfolder.iterdir():
         modifDate = datetime.datetime.fromtimestamp(os.path.getmtime(dir))
         if (datetime.date.today() - modifDate.date()).days > cutoff_limit:
@@ -26,7 +26,7 @@ def remove_empty_folders(workfolder: Path) -> None:
 
 
 def create_directory(directory: Path) -> None:
-    """Creates directory given its full path. Ignores OSError exceptions."""
+    """Creates directory given its full path. Ignores FileExistsError exceptions."""
     try:
         directory.mkdir()
         print('Created dir: %s' % (directory))
@@ -34,10 +34,10 @@ def create_directory(directory: Path) -> None:
         pass
 
 
-def archive_desktop_items(fromPath: Path, toPath: Path, exceptions: List[Path]):
-    """Archives any item from fromPath to toPath, unless in exceptions"""
-    for item in (i for i in fromPath.glob('*') if i not in exceptions):
-        item.rename(toPath / item.name)
+def archive_desktop_items(desktop: Path, workfolder: Path, exceptions: List[Path]) -> None:
+    """Archives any item from desktop to workfolder, unless in exceptions"""
+    for item in (i for i in desktop.glob('*') if i.name not in exceptions):
+        item.rename(workfolder / item.name)
 
 
 def main():
@@ -82,13 +82,13 @@ def main():
     )
     args = parser.parse_args()
 
-    workfolder = args.workfolder
-    workfolder_archive = workfolder / args.archive
     cutoff_limit = args.cutoff
     ds_fmt = args.ds_fmt
     desktop = args.desktop
     desktop_exceptions = args.exceptions.split(',')
-    workfolder_today = workfolder / str(datetime.date.today().strftime(ds_fmt))
+    workfolder = args.workfolder
+    workfolder_archive = workfolder / args.archive
+    workfolder_today = workfolder / datetime.date.today().strftime(ds_fmt)
 
     # Archive old folders
     archive_daily_workfolders(workfolder=workfolder, archive=workfolder_archive, cutoff_limit=cutoff_limit)
@@ -100,7 +100,7 @@ def main():
     create_directory(directory=workfolder_today)
 
     # Clean Desktop
-    archive_desktop_items(fromPath=desktop, toPath=workfolder_today, exceptions=desktop_exceptions)
+    archive_desktop_items(desktop=desktop, workfolder=workfolder_today, exceptions=desktop_exceptions)
 
 
 if __name__ == '__main__':
